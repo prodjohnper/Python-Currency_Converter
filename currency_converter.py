b128be5e7a1c5c5513eaf39b5ffe639bcdc8bd6a
Jonathan Pérez <45082645+prodjohnper@users.noreply.github.com>
2023-12-29 11:04:45 -0400

    Updated - Added amount input and calculator feature

--- currency_converter.py
+++ currency_converter.py
@@ -8,34 +8,47 @@ ON = True
 
 CURRENCIES = ['USD', 'EUR', 'JPY', 'GBP', 'AUD', 'MXN']
 
-def currency_converter(base):
+def currency_converter(base, amount=None):
     currencies = ','.join(CURRENCIES)
     url = f'{URL}&base_currency={base}&currencies={currencies}'
     try:
         response = requests.get(url)
-        data = response.json()
-        return data['data']
+        data = response.json()['data']
+        if amount is not None:
+            return {ticker: value * amount for ticker, value in data.items() if ticker != base}
+        else:
+            del data[base]
+            return data
     except:
         print('Invalid currency selection, try again.')
         print("Type 'Exit' to exit program")
         return None
 
 while ON:
-    base = input('\nEnter currency: ').upper()
-    if base == 'EXIT':
+    user_input = input('\nEnter currency: ').upper()
+    if user_input == 'EXIT':
         break
+
+    try:
+        base, *amount_str = user_input.split()
+        amount = float(amount_str[0]) if amount_str else None
+    except ValueError:
+        print('Invalid input format, try again.')
+        print("Type 'Exit' to exit program")
+        continue
+
     if base not in CURRENCIES:
-            print('Invalid currency selection, try again.')
-            print("Type 'Exit' to exit program")
-            continue
-    
-    data = currency_converter(base)
+        print('Invalid selection, try again.')
+        print("Type 'Exit' to exit program")
+        continue
+
+    data = currency_converter(base, amount)
     if not data:
         continue
-    del data[base]
-    
+
     for ticker, value in data.items():
         print(f'{ticker}: {value}')
+
     print("\nType 'Exit' to exit program")
 
 # Jonathan Perez - @prodjohnper
